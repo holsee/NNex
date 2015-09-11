2f32360a270b4b882db8c82894f6189db25736f4
holsee <sholdsworth@gmail.com>
2015-09-11 17:09:43 +0100

    added weights to connections

--- lib/simple_neuron.ex
+++ lib/simple_neuron.ex
@@ -2,23 +2,25 @@ defmodule SimpleNeuron do
   use GenServer
   require Logger
 
-  defstruct from_conns: [], to_conns: []
+  defstruct from_conns: [], to_conns: [], input_signals: []
 
   def start_link() do
     GenServer.start_link(SimpleNeuron, %SimpleNeuron{})
   end
 
-  def connect(neuron, to_neuron) do
+  def connect(neuron, to_neuron, weight) do
     {GenServer.call(neuron, {:connect, to_neuron}), 
-     GenServer.call(to_neuron, {:connected, neuron})}
+     GenServer.call(to_neuron, {:connected, neuron, weight})}
   end
 
   def signal(neuron, value) do
-    GenServer.cast(neuron, {:signal, value}) 
+    GenServer.cast(neuron, {:signal, value, self()}) 
   end
   
-  def forward_signal(%SimpleNeuron{to_conns: to_conns}, value) do
-    for conn <- to_conns, do: signal(conn, value)
+  defp forward_signal(%SimpleNeuron{to_conns: to_conns, input_signals: input_signals}, value) do 
+    #if Enum.count(to_conns) == Enum.count(input_signals) do 
+      for conn <- to_conns, do: signal(conn, value)
+    # end
   end
 
   # Callbacks
@@ -27,15 +29,36 @@ defmodule SimpleNeuron do
     {:reply, from, %SimpleNeuron{neuron|to_conns: [to_neuron|to_conns]}}
   end
 
-  def handle_call({:connected, from_neuron}, from, %SimpleNeuron{from_conns: from_conns} = neuron) do
-    {:reply, from, %SimpleNeuron{neuron|from_conns: [from_neuron|from_conns] }}
+  def handle_call({:connected, from_neuron, weight}, from, %SimpleNeuron{from_conns: from_conns} = neuron) do
+    {:reply, from, %SimpleNeuron{neuron|from_conns: [{key(from_neuron), weight}|from_conns] }}
   end  
 
-  def handle_cast({:signal, value}, %SimpleNeuron{} = neuron) do
-    Logger.info("Received signal #{value}")
+  def handle_cast({:signal, value, from}, %SimpleNeuron{from_conns: from_conns, input_signals: input_signals} = neuron) do
+    
+    weight = from_conns[key(from)] 
+
+    log_signal(value, from, weight)
+  
+    # TODO: perform computation when all inputs have arrived and then forward signal    
     forward_signal(neuron, value)
-    {:noreply, neuron} 
+    
+    {:noreply, %{neuron|input_signals: [{value, from}|input_signals]}} 
   end
 
+  # Helpers
+
+  defp key(pid) when is_pid(pid) do
+    pid 
+    |> :erlang.pid_to_list
+    |> :erlang.list_to_atom
+  end
+  
+  defp log_signal(value, from, nil) do
+    Logger.info("#{key(self())} Received signal #{value} from #{key(from)} input sensor")
+  end
+  defp log_signal(value, from, weight) do
+    Logger.info("#{key(self())} Received signal #{value} from #{key(from)} with connection weight #{weight}")
+  end
+ 
 end
 
